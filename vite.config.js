dba39d759289c9b6addd8b3fff330a00fa71818d
babysteps0524 <babysteps0524@gmail.com>
2026-08-14 17:13:53 +0900

    Fix Vite multi page build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
 import { defineConfig } from "vite";
 import UnoCSS from "unocss/vite";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = resolve(__filename, "..");
 
 export default defineConfig({
   plugins: [UnoCSS()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        stock: resolve(__dirname, "pages/stock.html"),
+        realestate: resolve(__dirname, "pages/realestate.html"),
+        taxSaving: resolve(__dirname, "pages/taxSaving.html"),
+        insurance: resolve(__dirname, "pages/insurance.html"),
+        computertax: resolve(__dirname, "pages/computertax.html"),
+      },
+    },
+  },
 
   server: {
     open: true,
